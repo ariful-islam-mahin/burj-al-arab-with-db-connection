231bc2376a0f95d0354e7bfb695e057ff4cc5c2c
Mahin <mahinyayaa@gmail.com>
2020-09-12 22:12:48 +0600

    burj-al-arab done

---
+++ src/firebase.config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDieNqg-hmbdi30E0nzG5Dh_JTs20KY3M8",
+    authDomain: "burj-al-arab2.firebaseapp.com",
+    databaseURL: "https://burj-al-arab2.firebaseio.com",
+    projectId: "burj-al-arab2",
+    storageBucket: "burj-al-arab2.appspot.com",
+    messagingSenderId: "229118337571",
+    appId: "1:229118337571:web:c3bf95f1db969eadb28796"
+  };
+
+  export default firebaseConfig 
